f18280e8927105d0201ac18343f8c228c5bf45c0
ushakiran-mannam <Ushakiran.Mannam@viacomcontractor.com>
2020-08-24 15:55:30 +0530

    Updated code

--- vars/s3upload.groovy
+++ vars/s3upload.groovy
@@ -1,5 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(String artifact , String bucketName){
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-local-keys', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+    
     sh "aws s3 cp $artifact s3://$bucketName"
+    }
+    
 }
